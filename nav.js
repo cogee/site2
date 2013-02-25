d229fe3fb2bb517dec14b3817bcc4b7991d5eaf7
Sam Cogan <cogansam@gmail.com>
2013-02-25 22:20:42 +0000

    Commit 3
    with jq nav working

--- nav.js
+++ nav.js
@@ -1,5 +1,41 @@
 $("#flip1").click(function () {
-$("#flip1").fadeOut(400);
-$("#flip11").delay(400).fadeIn(400);
+$("#flip1").slideToggle(400);
+$("#flip11").delay(400).show(400);
+
+
 });
 
+$("#flip11").click(function () {
+$("#flip11").hide(0);
+$("#flip1").slideToggle(400);
+
+});
+
+
+$("#flip2").click(function () {
+$("#flip2").slideToggle(400);
+$("#flip22").delay(400).show(400);
+
+
+});
+
+$("#flip22").click(function () {
+$("#flip22").hide(0);
+$("#flip2").slideToggle(400);
+
+});
+
+
+
+$("#flip3").click(function () {
+$("#flip3").slideToggle(400);
+$("#flip33").delay(400).show(400);
+
+
+});
+
+$("#flip33").click(function () {
+$("#flip33").hide(0);
+$("#flip3").slideToggle(400);
+
+});
